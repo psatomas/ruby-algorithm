2070ccaccc6f3a82c8142b6099547d495e008906
Tomás Araújo <psatomas@gmail.com>
2024-10-14 17:24:56 -0300

    feat: add each.rb

---
+++ each.rb
@@ -0,0 +1,10 @@
+nomes = ["joão", "maria", "josé", "mateus"]
+
+dict = {nome: "Diego", idade: 35, altura: 1.73}
+
+nome = "Diego"
+nomes.each do |nome|
+    puts nome
+end
+
+puts nome
--- while.rb
+++
@@ -1,8 +0,0 @@
-nomes = ["joão", "maria", "josé", "mateus"]
-
-count = 0
-while count < nomes.size do
-    puts nomes[count]
-    puts count
-    count += 1
-end
